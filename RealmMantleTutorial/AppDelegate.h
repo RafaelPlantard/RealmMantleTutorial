f6d9de368e68e9e9417a16fc596613941783aacd
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2015-12-28 11:08:45 -0200

    Initial Commit

---
+++ RealmMantleTutorial/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  RealmMantleTutorial
+//
+//  Created by Rafael Ferreira on 12/28/15.
+//  Copyright © 2015 Data Empire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ RealmMantleTutorial/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  RealmMantleTutorial
+//
+//  Created by Rafael Ferreira on 12/28/15.
+//  Copyright © 2015 Data Empire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
